493e498479efd6544b09c4b9ab30218087e88150
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-12-09 19:32:53 +0530

    ft: proper getTrades

--- contracts/TradeBook.sol
+++ contracts/TradeBook.sol
@@ -4,24 +4,33 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TradeBook as Ownable {
-
+contract TradeBook is Ownable {
     struct Trade {
-        id: string
-        user: address
-        amount: uint256
-        token0: address
-        token1: address
+        string id;
+        address user;
+        uint256 amount;
+        address token0;
+        address token1;
     }
-    
+
     mapping(address => Trade[]) public trades;
 
-    function addTrade(address _user, Trade _trade) public onlyOwner {
+    constructor() Ownable(msg.sender) {}
+
+    function addTrade(address _user, Trade memory _trade) public onlyOwner {
         trades[_user].push(_trade);
     }
 
-    function getTrades(address _user) public returns (Trade) {
-        return trades[_user];
-    }
+    function getTrades(address _user) public view returns (Trade[] memory) {
+        Trade[] memory _trades = new Trade[](trades[_user].length);
 
+        for (uint256 i = 0; i < trades[_user].length; ) {
+            _trades[i] = trades[_user][i];
+            unchecked {
+                i++;
+            }
+        }
+
+        return _trades;
+    }
 }
